8c27ef711e2d4e00962049bb33a479ea929e5f0b
fbwls <fbwls0218@gmail.com>
2024-02-19 15:05:52 +0900

    Add Sprint, Add Animation Blueprint
    Add Sprint Ability
    Adjust Animation Blueprint

---
+++ Source/RyuJinLibrary/Public/RyuJinAssetManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "RyuJinAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RYUJINLIBRARY_API URyuJinAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+};
